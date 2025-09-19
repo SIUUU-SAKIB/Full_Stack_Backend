cdfcebd148e1719aae9b13760d72db3dc44f078a
Sakib Islam <your-email@example.com>
2025-09-19 21:33:55 +0600

    Ad

--- src/app.ts
+++ src/app.ts
@@ -11,7 +11,7 @@ const app = express();
 
 app.use(
     cors({
-        origin: "https://ubiquitous-gecko-f5b739.netlify.app",
+        origin: "http://localhost:5173",
         credentials: true,
     })
 );
